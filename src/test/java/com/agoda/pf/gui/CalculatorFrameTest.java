a2985cdc67cbfe2b0a3fe41464f80719fe4f29aa
mthakur <manish.thakur@agoda.com>
2022-07-05 18:14:54 +0700

    move out common code

--- src/test/java/com/agoda/pf/gui/CalculatorFrameTest.java
+++ src/test/java/com/agoda/pf/gui/CalculatorFrameTest.java
@@ -1,21 +1,31 @@
 package com.agoda.pf.gui;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import javax.swing.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CalculatorFrameTest {
+    JPanel panel;
+    JFrame pfFrame;
+
+    @BeforeAll
+    public void setUp() {
+        panel = new JPanel();
+        pfFrame = new PFCalculatorFrame(panel);
+    }
+
     @Test
     public void containsPFPanel() {
-        JPanel panel = new JPanel();
         JFrame pfFrame = new PFCalculatorFrame(panel);
         Assertions.assertEquals(1, pfFrame.getComponents().length);
     }
+
     @Test
     public void setsDefaultWidthAndHeight() {
-        JPanel panel = new JPanel();
-        JFrame pfFrame = new PFCalculatorFrame(panel);
         Assertions.assertEquals(pfFrame.getWidth(), 300);
         Assertions.assertEquals(pfFrame.getHeight(), 400);
     }
