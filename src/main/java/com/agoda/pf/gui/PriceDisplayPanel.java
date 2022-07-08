f3d8035d3d0bcbf70a0623196942196f085eaf5d
mthakur <manish.thakur@agoda.com>
2022-07-08 14:42:08 +0700

    price display panel

---
+++ src/main/java/com/agoda/pf/gui/PriceDisplayPanel.java
@@ -0,0 +1,9 @@
+package com.agoda.pf.gui;
+
+import javax.swing.*;
+
+public class PriceDisplayPanel extends JPanel {
+    public PriceDisplayPanel(JLabel label) {
+        add(label);
+    }
+}
---
+++ src/test/java/com/agoda/pf/gui/PriceDisplayPanelTest.java
@@ -0,0 +1,14 @@
+package com.agoda.pf.gui;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+public class PriceDisplayPanelTest {
+    @Test
+    public void addsLabelToDisplayPrice() {
+        JPanel panel = new PriceDisplayPanel(new JLabel("0"));
+        Assertions.assertEquals("0", ((JLabel)panel.getComponents()[0]).getText());
+    }
+}
