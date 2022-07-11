0a6ab1ba3ff376e85e340e969fb91d76116b5cfd
mthakur <manish.thakur@agoda.com>
2022-07-11 14:18:32 +0700

    Input panel allows adding listeners

--- src/main/java/com/agoda/pf/gui/InputPanel.java
+++ src/main/java/com/agoda/pf/gui/InputPanel.java
@@ -1,12 +1,19 @@
 package com.agoda.pf.gui;
 
+
 import javax.swing.*;
+import javax.swing.event.DocumentListener;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputPanel extends JPanel {
+    private List<DocumentListener> listeners;
+
     public InputPanel() {
+        listeners = new ArrayList<>();
         setLayout(new GridLayout(3, 2));
         add(new JLabel("Frozen Price"));
         NumberFormatter formatter = new NumberFormatter(NumberFormat.getNumberInstance());
@@ -15,5 +22,13 @@ public class InputPanel extends JPanel {
         add(new JFormattedTextField(formatter));
         add(new JLabel("Max Cap"));
         add(new JFormattedTextField(formatter));
+    }
+
+    public void addInputChangeListeners(DocumentListener listener) {
+        listeners.add(listener);
+    }
+
+    public int getInputChangeListenersCount() {
+        return listeners.size();
     }
 }
--- src/test/java/com/agoda/pf/gui/InputPanelTest.java
+++ src/test/java/com/agoda/pf/gui/InputPanelTest.java
@@ -2,8 +2,10 @@ package com.agoda.pf.gui;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.swing.*;
+import javax.swing.event.DocumentListener;
 
 public class InputPanelTest {
     @Test
@@ -21,5 +23,12 @@ public class InputPanelTest {
         Assertions.assertEquals("NumberFormatter", ((JFormattedTextField) panel.getComponent(3)).getFormatter().getClass().getSimpleName());
         Assertions.assertEquals("Max Cap", ((JLabel) panel.getComponent(4)).getText());
         Assertions.assertEquals("NumberFormatter", ((JFormattedTextField) panel.getComponent(5)).getFormatter().getClass().getSimpleName());
+    }
+
+    @Test
+    public void allowsAddingListeners() {
+        InputPanel panel = new InputPanel();
+        panel.addInputChangeListeners(Mockito.mock(DocumentListener.class));
+        Assertions.assertEquals(1, panel.getInputChangeListenersCount());
     }
 }
