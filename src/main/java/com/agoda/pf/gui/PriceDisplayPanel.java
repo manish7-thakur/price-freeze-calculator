ae4b5d454ff959b504a7c36528d2dcc3c79546c7
mthakur <manish.thakur@agoda.com>
2022-07-11 17:32:04 +0700

    displaypanel uses border layout

--- src/main/java/com/agoda/pf/gui/PriceDisplayPanel.java
+++ src/main/java/com/agoda/pf/gui/PriceDisplayPanel.java
@@ -1,12 +1,14 @@
 package com.agoda.pf.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PriceDisplayPanel extends JPanel {
     private JLabel displayLabel;
 
     public PriceDisplayPanel() {
-        displayLabel = new JLabel("0");
+        setLayout(new BorderLayout());
+        displayLabel = new JLabel("0", SwingConstants.CENTER);
         add(displayLabel);
     }
 
--- src/test/java/com/agoda/pf/gui/PriceDisplayPanelTest.java
+++ src/test/java/com/agoda/pf/gui/PriceDisplayPanelTest.java
@@ -7,10 +7,12 @@ import javax.swing.*;
 
 public class PriceDisplayPanelTest {
     @Test
-    public void addsLabelToDisplayPrice() {
+    public void addsLabelToDisplayPriceAtBorderLayoutCenter() {
         JPanel panel = new PriceDisplayPanel();
-        Assertions.assertEquals("0", ((JLabel)panel.getComponents()[0]).getText());
+        Assertions.assertEquals("0", ((JLabel) panel.getComponents()[0]).getText());
+        Assertions.assertEquals("BorderLayout", panel.getLayout().getClass().getSimpleName());
     }
+
     @Test
     public void getAReferenceToDisplayPrice() {
         PriceDisplayPanel panel = new PriceDisplayPanel();
