538e12b1b6b3d6c7159182c064a0b882587532aa
mthakur <manish.thakur@agoda.com>
2022-07-11 13:34:37 +0700

    event listener calcualtes price & update it

--- src/main/java/com/agoda/pf/event/listener/InputChangeListener.java
+++ src/main/java/com/agoda/pf/event/listener/InputChangeListener.java
@@ -1,12 +1,20 @@
 package com.agoda.pf.event.listener;
 
+import com.agoda.pf.util.PFCalculator;
+import com.agoda.pf.util.PFResult;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 public class InputChangeListener implements DocumentListener {
 
     private final JLabel label;
+    private Double frozenPrice;
+    private double currentPrice;
+    private double deposit;
 
     public InputChangeListener(JLabel label) {
         this.label = label;
@@ -14,16 +22,47 @@ public class InputChangeListener implements DocumentListener {
 
     @Override
     public void insertUpdate(DocumentEvent e) {
-        label.setText("");
+        updatePrice(e.getDocument());
     }
 
     @Override
     public void removeUpdate(DocumentEvent e) {
-        label.setText("");
+        updatePrice(e.getDocument());
     }
 
     @Override
     public void changedUpdate(DocumentEvent e) {
 
+    }
+
+    public double getFrozenPrice() {
+        return frozenPrice;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public double getDeposit() {
+        return deposit;
+    }
+
+    private void updatePrice(Document d) {
+        String name = (String) d.getProperty("name");
+        if (name.equals("frozen-price")) frozenPrice = getPrice(d);
+        else if (name.equals("current-price")) currentPrice = getPrice(d);
+        else if (name.equals("deposit")) deposit = getPrice(d);
+        PFResult result = PFCalculator.calculate(frozenPrice, 0, 0);
+        label.setText("final price : " + result.getFinalPrice());
+    }
+
+    private Double getPrice(Document doc) {
+        double txt;
+        try {
+            txt = Double.parseDouble(doc.getText(0, doc.getLength()));
+        } catch (BadLocationException e) {
+            txt = 0;
+        }
+        return txt;
     }
 }
--- src/test/java/com/agoda/pf/event/listener/InputChangeListenerTest.java
+++ src/test/java/com/agoda/pf/event/listener/InputChangeListenerTest.java
@@ -1,5 +1,6 @@
 package com.agoda.pf.event.listener;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -7,32 +8,59 @@ import org.mockito.Mockito;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class InputChangeListenerTest {
     JLabel mockLabel;
-    DocumentListener listener;
+    InputChangeListener listener;
     DocumentEvent documentEvent;
+    Document mockDocument;
 
     @BeforeAll
     public void setUp() {
         mockLabel = Mockito.mock(JLabel.class);
         documentEvent = Mockito.mock(DocumentEvent.class);
+        mockDocument = Mockito.mock(Document.class);
         listener = new InputChangeListener(mockLabel);
     }
 
     @Test
-    public void updatesNewPriceOnInsert() {
+    public void updatesPriceParamsInsert() throws BadLocationException {
         Mockito.reset(mockLabel);
+        Mockito.when(documentEvent.getDocument()).thenReturn(mockDocument);
+        Mockito.when(mockDocument.getProperty("name")).thenReturn("frozen-price");
+        Mockito.when(mockDocument.getText(Mockito.anyInt(), Mockito.anyInt())).thenReturn("100.21");
         listener.insertUpdate(documentEvent);
-        Mockito.verify(mockLabel).setText("");
+        Assertions.assertEquals(100.21, listener.getFrozenPrice());
+        Mockito.when(mockDocument.getProperty("name")).thenReturn("current-price");
+        Mockito.when(mockDocument.getText(Mockito.anyInt(), Mockito.anyInt())).thenReturn("121.34");
+        listener.insertUpdate(documentEvent);
+        Assertions.assertEquals(121.34, listener.getCurrentPrice());
+        Mockito.when(mockDocument.getProperty("name")).thenReturn("deposit");
+        Mockito.when(mockDocument.getText(Mockito.anyInt(), Mockito.anyInt())).thenReturn("21.35");
+        listener.insertUpdate(documentEvent);
+        Assertions.assertEquals(21.35, listener.getDeposit());
+        Mockito.verify(mockLabel, Mockito.times(3)).setText(Mockito.anyString());
     }
 
     @Test
-    public void updatesNewPriceOnRemove() {
+    public void updatesNewPriceOnRemove() throws BadLocationException {
         Mockito.reset(mockLabel);
+        Mockito.when(documentEvent.getDocument()).thenReturn(mockDocument);
+        Mockito.when(mockDocument.getProperty("name")).thenReturn("frozen-price");
+        Mockito.when(mockDocument.getText(Mockito.anyInt(), Mockito.anyInt())).thenReturn("100.21");
         listener.removeUpdate(documentEvent);
-        Mockito.verify(mockLabel).setText("");
+        Assertions.assertEquals(100.21, listener.getFrozenPrice());
+        Mockito.when(mockDocument.getProperty("name")).thenReturn("current-price");
+        Mockito.when(mockDocument.getText(Mockito.anyInt(), Mockito.anyInt())).thenReturn("121.34");
+        listener.removeUpdate(documentEvent);
+        Assertions.assertEquals(121.34, listener.getCurrentPrice());
+        Mockito.when(mockDocument.getProperty("name")).thenReturn("deposit");
+        Mockito.when(mockDocument.getText(Mockito.anyInt(), Mockito.anyInt())).thenReturn("21.35");
+        listener.removeUpdate(documentEvent);
+        Assertions.assertEquals(21.35, listener.getDeposit());
+        Mockito.verify(mockLabel, Mockito.times(3)).setText(Mockito.anyString());
     }
 }
